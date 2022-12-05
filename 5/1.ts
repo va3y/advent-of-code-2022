1a5066c0980d36d481444cb94e22495287b508e3
Валерий Иванов <va.ivanov@cian.ru>
2022-12-05 23:18:42 +0300

    day 5

---
+++ 5/1.ts
@@ -0,0 +1,32 @@
+const input = await Deno.readTextFile("input.txt");
+const stacks = [
+	[],
+	["W", "B", "D", "N", "C", "F", "J"],
+	["P", "Z", "V", "Q", "L", "S", "T"],
+	["P", "Z", "B", "G", "J", "T"],
+	["D", "T", "L", "J", "Z", "B", "H", "C"],
+	["G", "V", "B", "J", "S"],
+	["P", "S", "Q"],
+	["B", "V", "D", "F", "L", "M", "P", "N"],
+	["P", "S", "M", "F", "B", "D", "L", "R"],
+	["V", "D", "T", "R"],
+];
+
+const moves = input
+	.split("\n\n")[1]
+	.split("\n")
+	.map((line) =>
+		line.match(/\d+/g)?.map((digit) => parseInt(digit))
+	) as number[][];
+
+for (const [amount, stackFrom, stackTo] of moves) {
+	for (let i = 0; i < amount; i++) {
+		const item = stacks[stackFrom]?.pop();
+
+		if (item) {
+			stacks[stackTo].push(item);
+		}
+	}
+}
+const answer = stacks.map((stack) => stack[stack.length - 1]);
+console.log(answer, answer.join(""));
