e08b42056006d2be66cffcb407fd5fa0c295bfda
Валерий Иванов <va.ivanov@cian.ru>
2022-12-15 00:15:10 +0300

    day 12 done

--- 12/2.ts
+++ 12/2.ts
@@ -4,32 +4,29 @@ const grid = input.split("\n").map((row) =>
   row.split("").map((char) => char.codePointAt(0) as number)
 );
 
-const start = grid.reduce((_acc, curr, y) => {
-  const x = curr.findIndex((char) => char === "S".codePointAt(0));
-  if (x !== -1) return { y, x };
-  return _acc;
-}, { x: 0, y: 0 });
+const start = grid.reduce((acc, curr, y) => {
+  const x = curr.findIndex((char) => char === "a".codePointAt(0));
+  if (x !== -1) acc.push({ x, y });
+  return acc;
+}, [] as { x: number; y: number }[]);
 const end = grid.reduce((_acc, curr, y) => {
   const x = curr.findIndex((char) => char === "E".codePointAt(0));
   if (x !== -1) return { y, x };
   return _acc;
 }, { x: 0, y: 0 });
 
-
-grid[start.y][start.x] = Infinity;
 grid[end.y][end.x] = Infinity;
 
-const paths: { x: number; y: number }[][] = [[start]];
+const paths: { x: number; y: number; id: number }[][] = [
+  ...start.map((point, i) => [{ ...point, id: i }]),
+];
 
 const visitedCells = new Set<string>();
+let answer = Infinity;
 
 while (paths.length) {
-  console.log(paths.length)
   const currentPath = paths[0];
   const lastCell = currentPath[currentPath.length - 1];
-
-  
-
 
   const directions = [
     { x: lastCell.x, y: lastCell.y + 1 },
@@ -39,24 +36,26 @@ while (paths.length) {
   ];
 
   for (const direction of directions) {
-    if (direction.x === end.x && direction.y === end.y && grid[lastCell.y]?.[lastCell.x] === 'z'.codePointAt(0)) {
-      console.log("answer: ", currentPath.length, currentPath);
+    if (
+      direction.x === end.x && direction.y === end.y &&
+      grid[lastCell.y]?.[lastCell.x] === "z".codePointAt(0)
+    ) {
+      answer = Math.min(answer, currentPath.length);
       break;
     }
 
-    
-
     if (
-      !visitedCells.has(`${direction.x}-${direction.y}`) &&
+      !visitedCells.has(`${lastCell.id}-${direction.x}-${direction.y}`) &&
       grid[direction.y]?.[direction.x] &&
       grid[direction.y][direction.x] <
         grid[lastCell.y][lastCell.x] + 2 as unknown as number
     ) {
-      visitedCells.add(`${direction.x}-${direction.y}`);
+      visitedCells.add(`${lastCell.id}-${direction.x}-${direction.y}`);
 
       paths.push([...JSON.parse(JSON.stringify(currentPath)), {
         x: direction.x,
         y: direction.y,
+        id: lastCell.id,
       }]);
     }
   }
@@ -64,3 +63,4 @@ while (paths.length) {
   paths.shift();
 }
 
+console.log("answer: ", answer);
