0a46d03e0c712fd145061ddf0edc9c36f21bcfaa
Валерий Иванов <va.ivanov@cian.ru>
2022-12-15 01:00:19 +0300

    tough luck

--- 13/1.ts
+++ 13/1.ts
@@ -9,12 +9,16 @@ console.log(pairs);
 type CompareType = number | CompareType[];
 
 function compare(aList: CompareType[], bList: CompareType[]): boolean {
-  for (const [index, a] of Object.entries(aList)) {
-    const b = bList[parseInt(index)];
-    if (a === undefined || b === undefined) return true;
+  for (let i = 0; i < Math.max(aList.length, bList.length); i++) {
+    const a = aList[i];
+    const b = bList[i];
+    if (a === undefined) return true;
+    if (b === undefined) return false;
+
     if (typeof a === "number" && typeof a === "number") {
       if (a !== b) return a < b;
     }
+
     if (Array.isArray(a) || Array.isArray(b)) {
       const res = compare(
         Array.isArray(a) ? a : [a],
@@ -27,6 +31,12 @@ function compare(aList: CompareType[], bList: CompareType[]): boolean {
   return true;
 }
 
+let count = 0;
+let answer = 0;
 for (const [a, b] of pairs) {
+  count++;
   console.log(compare(a, b));
+  if (compare(a, b)) answer += count;
 }
+// 1200 - low
+console.log(pairs.length, answer);
